25086df1d59132bac504c1467853e8e2ddfcc76a
wiickrz <will.cs.182@gmail.com>
2022-03-10 23:54:24 -0300

    Exercise solved

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,53 @@
-const choiceObj = new Map();
-choiceObj.set(0, "Pedra");
-choiceObj.set(1, "Papel");
-choiceObj.set(2, "Tesoura");
-const displayResult = document.querySelector("#result");
-const displayPlayerChoice = document.querySelector("#playerChoice");
-const displayComputerChoice = document.querySelector("#computerChoice");
-const drawScore = document.querySelector("#drawScore");
-const playerScore = document.querySelector("#playerScore");
-const computerScore = document.querySelector("#computerScore");
+let pcScore = 0;
+let playerScore = 0;
+let draws = 0;
 
-let drawPoints = 0;
-let playerPoints = 0;
-let computerPoints = 0;
-
-drawScore.innerHTML = drawPoints;
-
-let result = "Aguardando jogada";
-displayResult.innerHTML = result;
-
-let playerChoiceValue = "";
-displayPlayerChoice.innerHTML = playerChoiceValue;
-
-let computerChoiceValue = "";
-displayComputerChoice.innerHTML = computerChoiceValue;
-
-
-const getRandomIntInclusive = (min, max) => {
-    min = Math.ceil(0);
-    max = Math.floor(choiceObj.size);
-    const output = Math.floor(Math.random() * (max - min));
-    let computerChoiceValue = choiceObj.get(output);
-    displayComputerChoice.innerHTML = computerChoiceValue;
-    
-    const playerChoice = document.querySelector("li");
-    playerChoice.onclick = (e) => {
-        let playerChoiceValue = e.target.innerText;
-        displayPlayerChoice.innerHTML = playerChoiceValue;
-        
-        if (playerChoiceValue === computerChoiceValue) {
-            result = "Empate!";
-            drawPoints ++;
-            displayResult.innerHTML = result;
-            drawScore.innerHTML = drawPoints;
-        }
-    };
-    return result;
+let scoreBoard = document.getElementById("scoreBoard");
+let weaponsObj = {
+  Pedra: {
+    Pedra: "draw",
+    Papel: "lose",
+    Tesoura: "win",
+  },
+  Tesoura: {
+    Pedra: "lose",
+    Papel: "win",
+    Tesoura: "draw",
+  },
+  Papel: {
+    Pedra: "win",
+    Papel: "draw",
+    Tesoura: "lose",
+  },
 };
 
-const btnPedra = document.getElementById("btnPedra");
-btnPedra.addEventListener("click", getRandomIntInclusive);
+function checkValues(value) {
+  const choices = ["Pedra", "Papel", "Tesoura"];
+  const num = Math.floor(Math.random() * 3);
 
-const btnPapel = document.getElementById("btnPapel");
-btnPapel.addEventListener("click", getRandomIntInclusive);
+  let pcChoice = choices[num];
 
-const btnTesoura = document.getElementById("btnTesoura");
-btnTesoura.addEventListener("click", getRandomIntInclusive);
+  switch (weaponsObj[value][pcChoice]) {
+    case "win":
+      scoreBoard.style.cssText = "color: green";
+      scoreBoard.innerHTML = "Você ganhou!";
+      playerScore++;
+      break;
+    case "lose":
+      scoreBoard.style.cssText = "color: red";
+      scoreBoard.innerHTML = "Você perdeu!";
+      pcScore++;
+      break;
+    default:
+      scoreBoard.style.cssText = "color: gray";
+      scoreBoard.innerHTML = "Empate!";
+      draws++;
+      break;
+  }
 
-const btnReset = document.getElementById("btnReset");
-btnTesoura.addEventListener("click", resetScore);
+  document.getElementById("displayPlayer").innerHTML = value;
+  document.getElementById("displayPC").innerHTML = pcChoice;
+  document.getElementById("pcScore").innerHTML = pcScore;
+  document.getElementById("playerScore").innerHTML = playerScore;
+  document.getElementById("drawScore").innerHTML = draws;
+}
